dae2e1da07a45bed1f75bec9880d1d7101c0f739
rekteks <rekochan.dev@gmail.com>
2024-10-23 00:18:48 -0700

    Initial Commit

---
+++ ChatPrototype/ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by Robert Kochan on 10/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
